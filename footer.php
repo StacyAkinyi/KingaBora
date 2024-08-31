5fac7b5aa0ebd64d39f280ac69a4e64704ed39d6
StacyAkinyi <stacy.akinyi@gmail.com>
2024-08-31 19:21:00 +0300

    First home

---
+++ footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Footer</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div>    
+        <footer>
+            <div>
+                <h1>Kinga Bora</h1>
+                <section>LOGO</section>
+            </div>
+
+            <div class="foot">
+                <h3>Follow us on social media</h3>
+                <a href="https://www.facebook.com">Facebook</a>
+                <a href="https://www.twitter.com">Twitter</a>
+                <a href="https://www.instagram.com">Instagram</a>
+            </div>
+
+            <div class="foot">
+                <h3>Quick links</h3>
+                <a href="index.php">Home</a>
+                <a href="about.php">About Us</a>
+            </div>
+
+            <div class="foot">
+            <p>Contact us: <br>
+            Via email: kingabora@gmail.com <br>
+            Via mobile: +254010978654 
+            </p>
+            </div>
+
+            <div class="foot">
+                <p>&copy; 2020, All rights reserved</p>
+            </div>
+        </footer>
+        </div>
+
+    </body>
+</html>        
---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+            <nav>
+                <img>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="login.html">Login</a></li>
+                    <li><a href="signup.html">SignUp</a></li>
+                </ul>
+            </nav>
+        </header>
